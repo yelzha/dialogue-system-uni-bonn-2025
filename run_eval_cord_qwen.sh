e9f9cbc7a9ad33911e98ca907cf07115d0b733f3
yelzha <54392243+yelzha@users.noreply.github.com>
2025-07-02 20:13:00 +0200

    fix some 2.0

--- run_eval_cord_qwen.sh
+++ run_eval_cord_qwen.sh
@@ -12,12 +12,11 @@ module load Miniforge3
 module load CUDA/11.7.0
 module load git/2.41.0-GCCcore-12.3.0-nodocs
 
-# conda create -p /home/s06zyelt/dialogue-system-uni-bonn-2025/env python=3.10 -y
 source /software/easybuild-INTEL_A40/software/Miniforge3/24.1.2-0/etc/profile.d/conda.sh
 conda activate /home/s06zyelt/dialogue-system-uni-bonn-2025/env
 
-# pip install -r requirements.txt
-# pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117
+pip install -r requirements.txt
 
 # Run the evaluation script
 python src/evaluate_cord_qwen.py
